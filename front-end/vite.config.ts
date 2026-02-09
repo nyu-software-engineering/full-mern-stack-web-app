fbdc275709fa6ae0cfa9659f82202249514d0024
bloombar <2209098+bloombar@users.noreply.github.com>
2026-02-09 16:55:48 -0500

    alowJS false

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      //   viteTsconfigPaths(),
+      viteTsconfigPaths(),
       svgr({
         include: '**/*.svg?react',
       }),
